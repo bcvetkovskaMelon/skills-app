058bf4e3c5a66412d3f0041a95f5c4e390cde601
Biljana Cvetkovska <bcvetkovska@melontech.com>
2016-11-03 17:08:03 +0100

    adding files

---
+++ app/magic.js
@@ -0,0 +1,11 @@
+var Greeter = (function () {
+    function Greeter() {
+    }
+    Greeter.prototype.saySomething = function (message) {
+        if (message === void 0) { message = 'whats up'; }
+        return message + ' something';
+    };
+    return Greeter;
+}());
+var greeter = new Greeter();
+greeter.saySomething('hello');
